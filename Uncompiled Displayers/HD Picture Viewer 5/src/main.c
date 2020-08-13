e8a8b2ef99721231d0f4a8c02f7ecb7cb9bf326e
TheLastMillennial <contactTheLastM@gmail.com>
2020-08-13 18:53:22 -0500

    Replaced floats with integers
    Floats are bugy and will eventually cause major issues. Changed to ints instead but introduce resizing bug.

--- Uncompiled Displayers/HD Picture Viewer 5/src/main.c
+++ Uncompiled Displayers/HD Picture Viewer 5/src/main.c
@@ -173,7 +173,7 @@ void DrawImage(uint24_t picName, uint24_t maxWidth, uint24_t maxHeight){
   uint24_t i=0,widthSquares=0,heightSquares=0, maxWSquares=0,maxHSquares=0,widthPx=0,heightPx=0,xSquare=0, newWpx=0, newHpx=0,ySquare=0,xOffsetSquare=0,yOffsetSquare=0;
   uint16_t *palPtr[256];
   gfx_sprite_t *outputImg,*srcImg;
-  float scale=1;
+  uint24_t scale=1, scaleNum=1, scaleDen =1;
   gfx_FillScreen(0);
 
   //seeks past header (8bytes), imgName, and unselected images
@@ -198,285 +198,311 @@ void DrawImage(uint24_t picName, uint24_t maxWidth, uint24_t maxHeight){
   maxHSquares = (maxHeight/80);
   dbg_sprintf(dbgout,"maxWS: %d\nwidthS: %d\nmaxHS: %d\nheightS: %d\n",maxWSquares,widthSquares,maxHSquares,heightSquares);
 
-  //resizes image if it's too big and wont fit in user defined constraints
+  //NEW
+  if (widthSquares * maxHSquares < heightSquares * maxWSquares) {
+    scaleNum = maxWSquares;
+    scaleDen = widthSquares;
+  } else {
+    scaleNum = maxHSquares;
+    scaleDen = heightSquares;
+  }
+
+  /*
+  [jacobly] so now whenever we want to compute
+  `x * scale`
+  we instead want to compute
+  `x * (scaleNum / scaleDen)`
+  which can now be reordered to use strictly integer math as
+  `(x * scaleNum) / scaleDen`
+
+
+  [jacobly] if you don't know:
+  floorDiv(x, y) := x / y;
+  roundDiv(x, y) := (x + (y / 2)) / y;
+  ceilDiv(x, y) := (x + y - 1) / y;
+  [MateoC] huh I didn't know about roundDiv
+  */
+
+  /*OLD resizes image if it's too big and wont fit in user defined constraints
   if(widthSquares>maxWSquares){
-    dbg_sprintf(dbgout,"\nPath 1 ");
-    scale = (double)maxWSquares/(double)widthSquares;
-    if(heightSquares*scale>maxHSquares){
-      dbg_sprintf(dbgout,"\nPath 2 ");
+  dbg_sprintf(dbgout,"\nPath 1 ");
+  scale = (double)maxWSquares/(double)widthSquares;
+  if(heightSquares*scale>maxHSquares){
+  dbg_sprintf(dbgout,"\nPath 2 ");
 
-      scale = (double)maxHSquares/(double)heightSquares;
-    }
-  }else  if(heightSquares>maxHSquares){
-    dbg_sprintf(dbgout,"\nPath 3 ");
+  scale = (double)maxHSquares/(double)heightSquares;
+}
+}else  if(heightSquares>maxHSquares){
+dbg_sprintf(dbgout,"\nPath 3 ");
 
-    scale = (double)maxHSquares/(double)heightSquares;
-  }
-  dbg_sprintf(dbgout,"\nScale int: %d \nChanged WH %f",scale,((double)SQUARE_WIDTH_AND_HEIGHT*scale));
+scale = (double)maxHSquares/(double)heightSquares;
+}
+*/
+dbg_sprintf(dbgout,"\nScaleNum: %d \nscaleDen",scaleNum,scaleDen);
 
-    //allocates memory for outputImg according to scale
-    outputImg = gfx_MallocSprite((double)SQUARE_WIDTH_AND_HEIGHT*scale,(double)SQUARE_WIDTH_AND_HEIGHT*scale);
-    if (!outputImg){
-      PrintCenteredX("Failed to allocate memory!",130);
-      while(!os_GetCSC());
-      ti_CloseAll();
-      return;
-    }
-
-
-    //sets correct palettes
-    sprintf(palName, "HP%.2s0000\0",imgID);
-    palSlot = ti_Open(palName,"r");
-    if (!palSlot){
-      PrintCenteredX(palName,120);
-      PrintCenteredX("Palette does not exist!",130);
-      while(!os_GetCSC());
-      ti_CloseAll();
-      return;
-    }
-
-    //gfx_SetDrawBuffer();
-    ti_Seek(24,SEEK_SET,palSlot);
-    *palPtr = ti_GetDataPtr(palSlot);
-    gfx_SetPalette(*palPtr,512,0);
-    ti_Close(palSlot);
-
-    //Displays all the images
-    for(xSquare=0;xSquare<(widthSquares+1);xSquare++){
-      for(ySquare=0;ySquare<(heightSquares+1);ySquare++){
-        sprintf(searchName, "%.2s%03u%03u\0",imgID, xSquare, ySquare);//combines the separate parts into one name to search for
-
-        /*This opens the variable with the name that was just assembled.
-        * It then gets the pointer to that and stores it in a graphics variable
-        */
-        squareSlot = ti_Open(searchName,"r");
-        if (!squareSlot){
-          PrintCenteredX(searchName,120);
-          PrintCentered("Square doesn't exist!");
-          while(!os_GetCSC());
-          ti_CloseAll();
-          return;
-        }
-        //seeks past header
-        ti_Seek(16,SEEK_CUR,squareSlot);
-        //store the original image into srcImg
-        srcImg = (gfx_sprite_t*)ti_GetDataPtr(squareSlot);
-        //resizes it to outputImg size
-        gfx_ScaleSprite(srcImg,outputImg);
-
-        //displays the output image
-        gfx_ScaledSprite_NoClip(outputImg,xSquare*(SQUARE_WIDTH_AND_HEIGHT*scale),ySquare*(SQUARE_WIDTH_AND_HEIGHT*scale),1,1);
-
-        //cleans up
-        ti_Close(squareSlot);
-      }
-    }
-    free(outputImg);
-  }
+//allocates memory for outputImg according to scale
+outputImg = gfx_MallocSprite((SQUARE_WIDTH_AND_HEIGHT*scaleNum)/scaleDen,(SQUARE_WIDTH_AND_HEIGHT*scaleNum)/scaleDen);
+if (!outputImg){
+  PrintCenteredX("Failed to allocate memory!",130);
+  while(!os_GetCSC());
+  ti_CloseAll();
+  return;
+}
 
 
-  /* This UI keeps the user selection in the middle of the screen. */
-  void printNames(uint24_t startName, char *picNames, uint24_t numOfPics){
-    uint24_t i, Yoffset=0, y=0, curName=0;
+//sets correct palettes
+sprintf(palName, "HP%.2s0000\0",imgID);
+palSlot = ti_Open(palName,"r");
+if (!palSlot){
+  PrintCenteredX(palName,120);
+  PrintCenteredX("Palette does not exist!",130);
+  while(!os_GetCSC());
+  ti_CloseAll();
+  return;
+}
 
-    //clears old text and sets up for new text
-    gfx_SetTextScale(2,2);
-    gfx_SetColor(0);
-    gfx_FillRectangle_NoClip(0,0,140,240);
-    gfx_SetColor(255);
+//gfx_SetDrawBuffer();
+ti_Seek(24,SEEK_SET,palSlot);
+*palPtr = ti_GetDataPtr(palSlot);
+gfx_SetPalette(*palPtr,512,0);
+ti_Close(palSlot);
 
-    //re-draws UI lines
-    gfx_HorizLine_NoClip(0,120,6);
-    gfx_HorizLine_NoClip(136,120,5);
-    gfx_HorizLine_NoClip(6,110,130);
-    gfx_HorizLine_NoClip(6,130,130);
-    gfx_VertLine_NoClip(6,110,20);
-    gfx_VertLine_NoClip(136,110,21);
+//Displays all the images
+for(xSquare=0;xSquare<(widthSquares+1);xSquare++){
+  for(ySquare=0;ySquare<(heightSquares+1);ySquare++){
+    sprintf(searchName, "%.2s%03u%03u\0",imgID, xSquare, ySquare);//combines the separate parts into one name to search for
 
-    /*if the selected start name is under 4, that means we need to start drawing
-    * farther down the screen for the text to go in the right spot */
-    if(startName<4){
-      Yoffset = 75 - startName * Y_SPACING;
-      startName = 0;
-    }else{
-      startName-=4;
-    }
-    curName=startName;
-
-
-    /* draw the text on the screen. Starts displaying the name at element start
-    * then iterates until out of pics or about to draw off the screen */
-    for(i=0;i<numOfPics && y<180;i++){
-      //calculates where the text should be drawn
-      y = i * Y_SPACING + Y_MARGIN + Yoffset;
-
-      //Prints out the correct name
-      gfx_PrintStringXY(&picNames[curName++*BYTES_PER_IMAGE_NAME],X_MARGIN,y);
-      //while(!os_GetCSC());
-
-    }
-    //slows down scrolling speed
-    delay(150);
-  }
-
-
-
-  /* Rebuilds the database of images on the calculator*/
-  uint24_t rebuildDB(uint8_t p){
-    char *var_name, *imgInfo[16], nameBuffer[10];
-    uint8_t *search_pos = NULL;
-    uint24_t imagesFound=0;
-    char myData[8]="HDPALV1",names[8];
-    ti_var_t database = ti_Open("HDPICDB","w"), palette;
-    ti_Write("HDDATV10",8,1,database);//Rewrites the header because w overwrites everything
-
-    //resets splash screen for new loading SetLoadingBarProgress
-    SplashScreen();
-
-    /*
-    * Searches for palettes. This is a lot easier than searching for every single
-    * image square because there's is guarunteed to only be one palette per image.
-    * The palette containts all the useful information such as the image size and
-    * the two letter ID for each appvar. This makes it easy to find every square via a loop.
+    /*This opens the variable with the name that was just assembled.
+    * It then gets the pointer to that and stores it in a graphics variable
     */
-    while((var_name = ti_DetectVar(&search_pos, "HDPALV10", TI_APPVAR_TYPE)) != NULL) {
-      //sets progress of how many images were found
-      SetLoadingBarProgress(++imagesFound,MAX_IMAGES);
-      //finds the name, letter ID, and size of entire image this palette belongs to.
-      palette = ti_Open(var_name,"r");
-      //seeks past useless info
-      ti_Seek(8,SEEK_CUR,palette);
-      ti_Seek(16,SEEK_CUR,database);
-      //reads the important info (16 bytes)
-      ti_Read(&imgInfo,16,1,palette);
-      //Writes the info to the database
-      ti_Write(imgInfo,16,1,database);
-      //closes palette for next iteration
-      ti_Close(palette);
+    squareSlot = ti_Open(searchName,"r");
+    if (!squareSlot){
+      PrintCenteredX(searchName,120);
+      PrintCentered("Square doesn't exist!");
+      while(!os_GetCSC());
+      ti_CloseAll();
+      return;
     }
-    //closes the database
-    ti_Close(database);
-    gfx_End();
-    ti_SetArchiveStatus(true,database);
-    gfx_Begin();
-    SplashScreen();
-    gfx_SetTextXY(100,195);
-    gfx_PrintUInt(imagesFound,3);
-    if (imagesFound==0){
-      noImagesFound();
-    }
-    SetLoadingBarProgress(++p,TASKS_TO_FINISH);
+    //seeks past header
+    ti_Seek(16,SEEK_CUR,squareSlot);
+    //store the original image into srcImg
+    srcImg = (gfx_sprite_t*)ti_GetDataPtr(squareSlot);
+    //resizes it to outputImg size
+    gfx_ScaleSprite(srcImg,outputImg);
 
+    //displays the output image
+    gfx_ScaledSprite_NoClip(outputImg,xSquare*((SQUARE_WIDTH_AND_HEIGHT*scaleNum)/scaleDen),ySquare*((SQUARE_WIDTH_AND_HEIGHT*scaleNum)/scaleDen),1,1); //todo: get (SQUARE_WIDTH_AND_HEIGHT*scaleNum)/scaleDen) outside the loop
 
-
-    return imagesFound;
+    //cleans up
+    ti_Close(squareSlot);
   }
+}
+free(outputImg);
+}
 
-  void noImagesFound(){
-    gfx_SetTextFGColor(192);
-    PrintCenteredX("No Pictures Detected!",1);
-    gfx_SetTextFGColor(0);
-    PrintCenteredX("Convert some images and send them to your",11);
-    PrintCenteredX("calculator using the HDpic converter!",21);
-    PrintCenteredX("Tutorial:  https://youtu.be/s1-g8oSueQg",31);
-    PrintCenteredX("Press any key to quit",41);
-    return;
+
+/* This UI keeps the user selection in the middle of the screen. */
+void printNames(uint24_t startName, char *picNames, uint24_t numOfPics){
+  uint24_t i, Yoffset=0, y=0, curName=0;
+
+  //clears old text and sets up for new text
+  gfx_SetTextScale(2,2);
+  gfx_SetColor(0);
+  gfx_FillRectangle_NoClip(0,0,140,240);
+  gfx_SetColor(255);
+
+  //re-draws UI lines
+  gfx_HorizLine_NoClip(0,120,6);
+  gfx_HorizLine_NoClip(136,120,5);
+  gfx_HorizLine_NoClip(6,110,130);
+  gfx_HorizLine_NoClip(6,130,130);
+  gfx_VertLine_NoClip(6,110,20);
+  gfx_VertLine_NoClip(136,110,21);
+
+  /*if the selected start name is under 4, that means we need to start drawing
+  * farther down the screen for the text to go in the right spot */
+  if(startName<4){
+    Yoffset = 75 - startName * Y_SPACING;
+    startName = 0;
+  }else{
+    startName-=4;
   }
+  curName=startName;
 
-  //checks if the database is already created. If not, it creates it.
-  uint8_t databaseReady(){
-    char *var_name;
-    uint8_t *search_pos = NULL, exists=0, ready = 0;
-    ti_var_t myAppVar;
-    char myData[9]="HDDATV10"; //remember have one more space than text you're saving for null termiation
-    char compare[9]="HDDATV10";
-    //tries to find database using known header
-    while((var_name = ti_DetectVar(&search_pos, myData, TI_APPVAR_TYPE)) != NULL) {
-      exists=1;
-    }
-    //if file already exists, simply return
-    if (exists == 1)
-    ready = 2;
+
+  /* draw the text on the screen. Starts displaying the name at element start
+  * then iterates until out of pics or about to draw off the screen */
+  for(i=0;i<numOfPics && y<180;i++){
+    //calculates where the text should be drawn
+    y = i * Y_SPACING + Y_MARGIN + Yoffset;
+
+    //Prints out the correct name
+    gfx_PrintStringXY(&picNames[curName++*BYTES_PER_IMAGE_NAME],X_MARGIN,y);
+    //while(!os_GetCSC());
+
+  }
+  //slows down scrolling speed
+  delay(150);
+}
+
+
+
+/* Rebuilds the database of images on the calculator*/
+uint24_t rebuildDB(uint8_t p){
+  char *var_name, *imgInfo[16], nameBuffer[10];
+  uint8_t *search_pos = NULL;
+  uint24_t imagesFound=0;
+  char myData[8]="HDPALV1",names[8];
+  ti_var_t database = ti_Open("HDPICDB","w"), palette;
+  ti_Write("HDDATV10",8,1,database);//Rewrites the header because w overwrites everything
+
+  //resets splash screen for new loading SetLoadingBarProgress
+  SplashScreen();
+
+  /*
+  * Searches for palettes. This is a lot easier than searching for every single
+  * image square because there's is guarunteed to only be one palette per image.
+  * The palette containts all the useful information such as the image size and
+  * the two letter ID for each appvar. This makes it easy to find every square via a loop.
+  */
+  while((var_name = ti_DetectVar(&search_pos, "HDPALV10", TI_APPVAR_TYPE)) != NULL) {
+    //sets progress of how many images were found
+    SetLoadingBarProgress(++imagesFound,MAX_IMAGES);
+    //finds the name, letter ID, and size of entire image this palette belongs to.
+    palette = ti_Open(var_name,"r");
+    //seeks past useless info
+    ti_Seek(8,SEEK_CUR,palette);
+    ti_Seek(16,SEEK_CUR,database);
+    //reads the important info (16 bytes)
+    ti_Read(&imgInfo,16,1,palette);
+    //Writes the info to the database
+    ti_Write(imgInfo,16,1,database);
+    //closes palette for next iteration
+    ti_Close(palette);
+  }
+  //closes the database
+  ti_Close(database);
+  gfx_End();
+  ti_SetArchiveStatus(true,database);
+  gfx_Begin();
+  SplashScreen();
+  gfx_SetTextXY(100,195);
+  gfx_PrintUInt(imagesFound,3);
+  if (imagesFound==0){
+    noImagesFound();
+  }
+  SetLoadingBarProgress(++p,TASKS_TO_FINISH);
+
+
+
+  return imagesFound;
+}
+
+void noImagesFound(){
+  gfx_SetTextFGColor(192);
+  PrintCenteredX("No Pictures Detected!",1);
+  gfx_SetTextFGColor(0);
+  PrintCenteredX("Convert some images and send them to your",11);
+  PrintCenteredX("calculator using the HDpic converter!",21);
+  PrintCenteredX("Tutorial:  https://youtu.be/s1-g8oSueQg",31);
+  PrintCenteredX("Press any key to quit",41);
+  return;
+}
+
+//checks if the database is already created. If not, it creates it.
+uint8_t databaseReady(){
+  char *var_name;
+  uint8_t *search_pos = NULL, exists=0, ready = 0;
+  ti_var_t myAppVar;
+  char myData[9]="HDDATV10"; //remember have one more space than text you're saving for null termiation
+  char compare[9]="HDDATV10";
+  //tries to find database using known header
+  while((var_name = ti_DetectVar(&search_pos, myData, TI_APPVAR_TYPE)) != NULL) {
+    exists=1;
+  }
+  //if file already exists, simply return
+  if (exists == 1)
+  ready = 2;
+  else{
+    //if file doesn't already exist, create it.
+    //creates the database appvar and writes the header. Checks if wrote successfuly
+    myAppVar=ti_Open("HDPICDB", "w");
+    if(!myAppVar)
+    ready = 3;
+    if(ti_Write(&myData,8,1,myAppVar)!=1)
+    ready = 4;
+    if (ti_Rewind(myAppVar) == EOF)
+    ready = 5;
+    if (ti_Read(&myData,8, 1, myAppVar) != 1)
+    ready = 6;
+    if (strcmp(myData,compare)!=0)
+    ready = 7;
     else{
-      //if file doesn't already exist, create it.
-      //creates the database appvar and writes the header. Checks if wrote successfuly
-      myAppVar=ti_Open("HDPICDB", "w");
-      if(!myAppVar)
-      ready = 3;
-      if(ti_Write(&myData,8,1,myAppVar)!=1)
-      ready = 4;
-      if (ti_Rewind(myAppVar) == EOF)
-      ready = 5;
-      if (ti_Read(&myData,8, 1, myAppVar) != 1)
-      ready = 6;
-      if (strcmp(myData,compare)!=0)
-      ready = 7;
-      else{
-        ready = 1;
-      }
+      ready = 1;
     }
-    ti_CloseAll();
-
-    //checks what happened
-    if(ready==1){
-      gfx_SetTextFGColor(195);
-      PrintCenteredX("created",180);
-      return 1;
-    }else if(ready==2){
-      gfx_SetTextFGColor(004);
-      PrintCenteredX("exists",180);
-      return 2;
-    }else{
-      gfx_SetTextFGColor(224);
-      PrintCenteredX("failure",180);
-      gfx_SetTextXY(120,190);
-      gfx_PrintUInt(ready,1);
-      return 0;
-    }
-
-
   }
+  ti_CloseAll();
 
-  //makes a loading bar and fills it in depending on progress made (p) / tasks left (t)
-  void SetLoadingBarProgress(uint8_t p, uint8_t t){
-    p=((double)p/(double)t)*200.0;
-    //ensures loading bar doesn't go past max point
-    if (p>200)
-    p=200;
-
-    gfx_SetColor(128);
-    gfx_FillRectangle_NoClip(60,153,(uint8_t)p,7);
-
-  }
-
-  //creates a simple splash screen when program starts
-  void SplashScreen(){
-    //sets color to grey
-    gfx_SetColor(181);
-    gfx_FillRectangle_NoClip(60,80,LCD_WIDTH-120,LCD_HEIGHT-160);
-    /* Print title screen */
-    PrintCentered("HD Picture Viewer");
-  }
-
-  /* Prints a screen centered string */
-  void PrintCentered(const char *str)
-  {
-    gfx_PrintStringXY(str,(LCD_WIDTH - gfx_GetStringWidth(str)) / 2, (LCD_HEIGHT - 8) / 2);
-  }
-  /* Prints a X centered string */
-  void PrintCenteredX(const char *str, uint8_t y)
-  {
-    gfx_PrintStringXY(str, (LCD_WIDTH - gfx_GetStringWidth(str)) / 2, y);
-  }
-  /* Prints a Y centered string */
-  void PrintCenteredY(const char *str, uint8_t x)
-  {
-    gfx_PrintStringXY(str, x, (LCD_HEIGHT - 8) / 2);
+  //checks what happened
+  if(ready==1){
+    gfx_SetTextFGColor(195);
+    PrintCenteredX("created",180);
+    return 1;
+  }else if(ready==2){
+    gfx_SetTextFGColor(004);
+    PrintCenteredX("exists",180);
+    return 2;
+  }else{
+    gfx_SetTextFGColor(224);
+    PrintCenteredX("failure",180);
+    gfx_SetTextXY(120,190);
+    gfx_PrintUInt(ready,1);
+    return 0;
   }
 
 
-  /* Draw text on the homescreen at the given X/Y location */
-  void printText(int8_t xpos, int8_t ypos, const char *text) {
-    os_SetCursorPos(ypos, xpos);
-    os_PutStrFull(text);
-  }
+}
+
+//makes a loading bar and fills it in depending on progress made (p) / tasks left (t)
+void SetLoadingBarProgress(uint8_t p, uint8_t t){
+  p=((double)p/(double)t)*200.0;
+  //ensures loading bar doesn't go past max point
+  if (p>200)
+  p=200;
+
+  gfx_SetColor(128);
+  gfx_FillRectangle_NoClip(60,153,(uint8_t)p,7);
+
+}
+
+//creates a simple splash screen when program starts
+void SplashScreen(){
+  //sets color to grey
+  gfx_SetColor(181);
+  gfx_FillRectangle_NoClip(60,80,LCD_WIDTH-120,LCD_HEIGHT-160);
+  /* Print title screen */
+  PrintCentered("HD Picture Viewer");
+}
+
+/* Prints a screen centered string */
+void PrintCentered(const char *str)
+{
+  gfx_PrintStringXY(str,(LCD_WIDTH - gfx_GetStringWidth(str)) / 2, (LCD_HEIGHT - 8) / 2);
+}
+/* Prints a X centered string */
+void PrintCenteredX(const char *str, uint8_t y)
+{
+  gfx_PrintStringXY(str, (LCD_WIDTH - gfx_GetStringWidth(str)) / 2, y);
+}
+/* Prints a Y centered string */
+void PrintCenteredY(const char *str, uint8_t x)
+{
+  gfx_PrintStringXY(str, x, (LCD_HEIGHT - 8) / 2);
+}
+
+
+/* Draw text on the homescreen at the given X/Y location */
+void printText(int8_t xpos, int8_t ypos, const char *text) {
+  os_SetCursorPos(ypos, xpos);
+  os_PutStrFull(text);
+}
