2c8eccd8224da8b7aad60746321727cd985b3373
TheLastMillennial <35713276+TheLastMillennial@users.noreply.github.com>
2026-03-17 19:50:06 -0500

    Moved half-res code to gfxCompatability.h

--- src/gfxCompatibility.h
+++ src/gfxCompatibility.h
@@ -24,13 +24,130 @@ private:
 	inline static char cPalette[9] = "";
 	inline static uint24_t iPaletteEntries = 0;
 	inline static bool bGIFmode = false;
+	inline static bool bHalfResMode = false;
+
+	static void SetHalfResMode()
+	{
+		typedef struct lcd_timing
+		{
+			uint32_t : 2;
+			uint32_t PPL : 6;
+			uint32_t HSW : 8;
+			uint32_t HFP : 8;
+			uint32_t HBP : 8;
+
+			uint32_t LPP : 10;
+			uint32_t VSW : 6;
+			uint32_t VFP : 8;
+			uint32_t VBP : 8;
+
+			uint32_t PCD_LO : 5;
+			uint32_t CLKSEL : 1;
+			uint32_t ACB : 5;
+			uint32_t IVS : 1;
+			uint32_t IHS : 1;
+			uint32_t IPC : 1;
+			uint32_t IOE : 1;
+			uint32_t : 1;
+			uint32_t CPL : 10;
+			uint32_t BCD : 1;
+			uint32_t PCD_HI : 5;
+		} lcd_timing_t;
+
+		typedef struct res_settings
+		{
+			uint8_t frctrl;
+			uint8_t bp;
+			uint16_t xe;
+			uint8_t tfa;
+			uint8_t ramctrl1;
+			lcd_timing_t timing;
+		} res_settings_t;
+
+		static res_settings_t settings[2] =
+		{
+			{
+				/* Default ST7789 settings */
+				.frctrl = LCD_FRCTRL_DEFAULT,
+				.bp = LCD_BP_DEFAULT,
+				.xe = LCD_WIDTH - 1,
+				.tfa = 0,
+				.ramctrl1 = LCD_RAMCTRL1_DEFAULT
+			},
+			{
+				/* With the following ST7789 timing:
+				 * Refreshes LCD in at most 16.51 ms after VSYNC, assuming worst case 9.5 MHz clock
+				 * Waits at least 3.42 ms after VSYNC to read LCD memory, assuming worst case 10.5 MHz clock
+				 */
+				.frctrl = LCD_RTN_378 | LCD_NL_DEFAULT, /* 378 clocks per line */
+				.bp = 95, /* 95 lines of back porch */
+				.xe = HALF_LCD_WIDTH - 1,
+				.tfa = HALF_LCD_WIDTH,
+				.ramctrl1 = LCD_DM_VSYNC | LCD_RAM_DEFAULT,
+				/* With the following PL111 timing:
+				 * Refreshes LCD at 60 Hz = 24 MHz / (800*250*2), a VSYNC period of 16.67 ms
+				 * Outputs 38400 pixels to LCD memory within the first 3.40 ms after VSYNC
+				 */
+				.timing =
+				{
+					.PPL = 768 / 16 - 1, /* 768 pixels per line */
+					.HSW = 1 - 1,
+					.HFP = (800 - 768 - 1 - 1) - 1, /* 800 total clocks per line */
+					.HBP = 1 - 1,
+					.LPP = HALF_LCD_WIDTH * LCD_HEIGHT / 768, /* 50 lines */
+					.VSW = 1 - 1,
+					.VFP = 250 - (HALF_LCD_WIDTH * LCD_HEIGHT / 768) - 1, /* 250 total lines */
+					.VBP = 0,
+					.PCD_LO = (2 - 2) & 0x1F, /* clock divisor of 2 */
+					.CLKSEL = 0,
+					.ACB = 0,
+					.IVS = 1,
+					.IHS = 1,
+					.IPC = 1,
+					.IOE = 1,
+					.CPL = 768 - 1,
+					.BCD = 0,
+					.PCD_HI = (2 - 2) >> 5
+				}
+			}
+		};
+
+		const res_settings_t *p = &settings[bHalfResMode];
+
+		/* Initialize LCD driver */
+		lcd_Init();
+
+		/* Set clocks per line */
+		lcd_SetNormalFrameRateControl(p->frctrl);
+		/* Set back porch */
+		lcd_SetNormalBackPorchControl(p->bp);
+		/* Set horizontal output window */
+		lcd_SetColumnAddress(0, p->xe);
+		/* Set fixed left scroll area */
+		lcd_SetScrollArea(p->tfa, LCD_WIDTH - p->tfa, 0);
+		/* Set starting vertical scroll address to 0 */
+		lcd_SetScrollAddress(0);
+		/* Set display mode */
+		lcd_SetRamInterface(p->ramctrl1);
+		/* Set interlace mode */
+		lcd_SetInterlacedMode(bHalfResMode);
+
+		/* Save old display timing when enabling */
+		if (bHalfResMode) {
+			memcpy(&settings[0].timing, (const void *)&lcd_Timing0, sizeof(settings[0].timing));
+		}
+		/* Set display timing */
+		memcpy((void *)&lcd_Timing0, &p->timing, sizeof(p->timing));
+
+		/* Cleanup LCD driver */
+		lcd_Cleanup();
+	}
 
 
 public:
-	// Static method to get the instance of the Singleton
 	static HDpicGFX &getInstance()
 	{
-		static HDpicGFX instance; // Guaranteed to be created once
+		static HDpicGFX instance; 
 		return instance;
 	}
 
@@ -42,8 +159,28 @@ public:
 
 	static void usePictureMode()
 	{
-		paletteHeaderSize = IMG_PALETTE_HEADER_SIZE;
 		bGIFmode = false;
+		paletteHeaderSize = IMG_PALETTE_HEADER_SIZE;
+	}
+
+	// Sets LCD timings/settings to use 160 x 120
+	static void useHalfResMode()
+	{
+		if (!bHalfResMode)
+		{
+			bHalfResMode = true;
+			SetHalfResMode();
+		}
+	}
+
+	// Sets LCD timings/settings to use 320 x 240 (default)
+	static void useFullResMode()
+	{
+		if (bHalfResMode)
+		{
+			bHalfResMode = false;
+			SetHalfResMode();
+		}
 	}
 
 	//Returns true if gfx16 library in use
@@ -52,6 +189,7 @@ public:
 		return b16bppModeEnabled;
 	}
 
+	//Returns size of palette header in bytes
 	static uint8_t getPaletteHeaderSize()
 	{
 		return paletteHeaderSize;
@@ -174,8 +312,6 @@ public:
 	// Draw clipped or unclipped sprite. Uses currently set bpp mode.
 	static void sprite(gfx_sprite_t *picture, uint24_t x, uint24_t y, bool bClipPicture = true)
 	{
-		//dbg_sprintf(dbgout, "\n Drawing... %d x %d", picture->width, picture->height);
-
 		if (bClipPicture) {
 			if (is16bppMode())
 				gfx16_Sprite(picture, x, y);
@@ -208,7 +344,6 @@ public:
 	}
 
 	// Copies portion of screen from one place to another. Uses currently set bpp mode.
-	// WARNING: gfx16 library has a visual but when copying screen to the right.
 	static void copyRectangle(uint24_t srcX, uint24_t srcY, uint24_t dstX, uint24_t dstY, uint24_t width, uint8_t height, gfx_location_t srcBuffer = gfx_screen, gfx_location_t dstBuffer = gfx_buffer)
 	{
 		if (is16bppMode())
--- src/main.cpp
+++ src/main.cpp
@@ -29,124 +29,6 @@
 #include "guiUtils.h"
 #include "hdlib/hdlib.h"
 
-void SetHalfResMode(bool enable)
-{
-	typedef struct lcd_timing
-	{
-		uint32_t : 2;
-		uint32_t PPL : 6;
-		uint32_t HSW : 8;
-		uint32_t HFP : 8;
-		uint32_t HBP : 8;
-
-		uint32_t LPP : 10;
-		uint32_t VSW : 6;
-		uint32_t VFP : 8;
-		uint32_t VBP : 8;
-
-		uint32_t PCD_LO : 5;
-		uint32_t CLKSEL : 1;
-		uint32_t ACB : 5;
-		uint32_t IVS : 1;
-		uint32_t IHS : 1;
-		uint32_t IPC : 1;
-		uint32_t IOE : 1;
-		uint32_t : 1;
-		uint32_t CPL : 10;
-		uint32_t BCD : 1;
-		uint32_t PCD_HI : 5;
-	} lcd_timing_t;
-
-	typedef struct res_settings
-	{
-		uint8_t frctrl;
-		uint8_t bp;
-		uint16_t xe;
-		uint8_t tfa;
-		uint8_t ramctrl1;
-		lcd_timing_t timing;
-	} res_settings_t;
-
-	static res_settings_t settings[2] =
-	{
-		{
-			/* Default ST7789 settings */
-			.frctrl = LCD_FRCTRL_DEFAULT,
-			.bp = LCD_BP_DEFAULT,
-			.xe = LCD_WIDTH - 1,
-			.tfa = 0,
-			.ramctrl1 = LCD_RAMCTRL1_DEFAULT
-		},
-		{
-			/* With the following ST7789 timing:
-			 * Refreshes LCD in at most 16.51 ms after VSYNC, assuming worst case 9.5 MHz clock
-			 * Waits at least 3.42 ms after VSYNC to read LCD memory, assuming worst case 10.5 MHz clock
-			 */
-			.frctrl = LCD_RTN_378 | LCD_NL_DEFAULT, /* 378 clocks per line */
-			.bp = 95, /* 95 lines of back porch */
-			.xe = HALF_LCD_WIDTH - 1,
-			.tfa = HALF_LCD_WIDTH,
-			.ramctrl1 = LCD_DM_VSYNC | LCD_RAM_DEFAULT,
-			/* With the following PL111 timing:
-			 * Refreshes LCD at 60 Hz = 24 MHz / (800*250*2), a VSYNC period of 16.67 ms
-			 * Outputs 38400 pixels to LCD memory within the first 3.40 ms after VSYNC
-			 */
-			.timing =
-			{
-				.PPL = 768 / 16 - 1, /* 768 pixels per line */
-				.HSW = 1 - 1,
-				.HFP = (800 - 768 - 1 - 1) - 1, /* 800 total clocks per line */
-				.HBP = 1 - 1,
-				.LPP = HALF_LCD_WIDTH * LCD_HEIGHT / 768, /* 50 lines */
-				.VSW = 1 - 1,
-				.VFP = 250 - (HALF_LCD_WIDTH * LCD_HEIGHT / 768) - 1, /* 250 total lines */
-				.VBP = 0,
-				.PCD_LO = (2 - 2) & 0x1F, /* clock divisor of 2 */
-				.CLKSEL = 0,
-				.ACB = 0,
-				.IVS = 1,
-				.IHS = 1,
-				.IPC = 1,
-				.IOE = 1,
-				.CPL = 768 - 1,
-				.BCD = 0,
-				.PCD_HI = (2 - 2) >> 5
-			}
-		}
-	};
-
-	const res_settings_t *p = &settings[enable];
-
-	/* Initialize LCD driver */
-	lcd_Init();
-
-	/* Set clocks per line */
-	lcd_SetNormalFrameRateControl(p->frctrl);
-	/* Set back porch */
-	lcd_SetNormalBackPorchControl(p->bp);
-	/* Set horizontal output window */
-	lcd_SetColumnAddress(0, p->xe);
-	/* Set fixed left scroll area */
-	lcd_SetScrollArea(p->tfa, LCD_WIDTH - p->tfa, 0);
-	/* Set starting vertical scroll address to 0 */
-	lcd_SetScrollAddress(0);
-	/* Set display mode */
-	lcd_SetRamInterface(p->ramctrl1);
-	/* Set interlace mode */
-	lcd_SetInterlacedMode(enable);
-
-	/* Save old display timing when enabling */
-	if (enable) {
-		memcpy(&settings[0].timing, (const void *)&lcd_Timing0, sizeof(settings[0].timing));
-	}
-	/* Set display timing */
-	memcpy((void *)&lcd_Timing0, &p->timing, sizeof(p->timing));
-
-	/* Cleanup LCD driver */
-	lcd_Cleanup();
-}
-
-
 
 int main(void)
 {
@@ -526,6 +408,7 @@ uint8_t drawGIF(uint24_t picName, bool fullScreenPic)
 	KeyPressHandler &keyHandler = KeyPressHandler::getInstance();
 	HDpicGFX &gfx = HDpicGFX::getInstance();
 	HDpicGFX::useGIFMode();
+	
 	if (fullScreenPic) {
 		HDpicGFX::use8bpp(); //GIF is always 8bpp
 	}
@@ -605,7 +488,7 @@ uint8_t drawGIF(uint24_t picName, bool fullScreenPic)
 
 	// Cover up the last image
 	gfx_FillScreen(GIF_BACKGROUND_COLOR);
-	SetHalfResMode(true);
+	HDpicGFX::useHalfResMode();
 
 	//dbg_WatchpointSet(*srcGif, 1, DBG_WATCHPOINT_ALL);
 	lz4_Decompress(*srcGif, curPicture.framesPtrList[curFrame]);//pre-decompress first frame
@@ -621,7 +504,7 @@ uint8_t drawGIF(uint24_t picName, bool fullScreenPic)
 			if (++next > finalFrame)
 				next = 0;
 			if (keyHandler.scanKeys(fullScreenPic)) {
-				SetHalfResMode(false);
+				HDpicGFX::useFullResMode();
 				return 0;
 			}
 		} while (frames[next] == nullptr);
@@ -638,7 +521,7 @@ uint8_t drawGIF(uint24_t picName, bool fullScreenPic)
 		frameTimer = clock();
 		curFrame = next;
 	}
-	SetHalfResMode(false);
+	HDpicGFX::useFullResMode();
 	return 0;
 }
 
